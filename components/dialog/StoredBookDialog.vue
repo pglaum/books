89e549a9019b9785797fffd25d2d3881ebc3085a
Philipp Glaum <philipp.glaum@sinetcon.com>
2024-05-07 19:06:17 +0200

    update StoredBookDialog

--- components/dialog/StoredBookDialog.vue
+++ components/dialog/StoredBookDialog.vue
@@ -11,145 +11,190 @@
         </template>
 
         <template #body>
-            <div class="flex items-center justify-center">
-                <img
+            <div class="flex flex-col items-start justify-start gap-4 sm:flex-row">
+                <div
                     v-if="vi.imageLinks?.thumbnail"
-                    :src="vi.imageLinks?.thumbnail"
-                    class="float-left me-2 h-48"
+                    class="aspect-[3/4] h-48"
                 >
-            </div>
-
-            <div class="grid gap-2">
-                <div
-                    v-if="book.list === BookListEnum.Values.LIBRARY"
-                    class="flex items-center gap-4"
-                >
-                    <div class="flex items-center gap-2 text-success">
-                        <CheckCircle class="size-4" />
-                        This book is in your library
-                    </div>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        :disabled="isLoading"
-                        @click="setList(BookListEnum.Values.WISHLIST)"
+                    <img
+                        :src="vi.imageLinks?.thumbnail"
+                        class="size-full object-contain object-center"
                     >
-                        Move to wishlist
-                    </Button>
                 </div>
-                <div
-                    v-else-if="book.list === BookListEnum.Values.WISHLIST"
-                    class="flex items-center gap-4"
-                >
-                    <div class="flex items-center gap-2 text-muted-foreground">
-                        <Scroll class="size-4" />
-                        This book is on your wishlist
-                    </div>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        :disabled="isLoading"
-                        @click="setList(BookListEnum.Values.LIBRARY)"
-                    >
-                        Add to library
-                    </Button>
-                </div>
-                <div
-                    v-if="book.is_read"
-                    class="flex items-center gap-4"
-                >
-                    <div class="flex items-center gap-2 text-success">
-                        <BookCheck class="size-4" />
-                        You've read the book
-                    </div>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        :disabled="isLoading"
-                        @click="readBook(false)"
-                    >
-                        Mark as unread
-                    </Button>
-                </div>
-                <div
-                    v-else
-                    class="flex items-center gap-4"
-                >
-                    <div class="flex items-center gap-2 text-muted-foreground">
-                        <BookDashed class="size-4" />
-                        You haven't read this book yet
-                    </div>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        :disabled="isLoading"
-                        @click="readBook(true)"
-                    >
-                        Mark as read
-                    </Button>
-                </div>
-                <div class="flex items-center gap-4">
-                    <Button
-                        variant="destructive-outline"
-                        size="sm"
-                        @click="showReallyDelete = true"
-                    >
-                        <Trash2 class="size-4" />
-                        Delete
-                    </Button>
+                <div class="grid gap-2">
                     <div
-                        v-if="showReallyDelete"
-                        class="flex items-center gap-2"
+                        class="flex items-center gap-4"
                     >
-                        Do you really want to delete this book?
-                        <Button
-                            variant="destructive-outline"
-                            size="sm"
-                            :disabled="isLoading"
-                            @click="removeBook()"
+                        <div
+                            v-if="book.list === BookListEnum.Values.LIBRARY"
+                            class="flex items-center gap-2 text-success"
                         >
-                            Yes
-                        </Button>
+                            <CheckCircle class="size-4" />
+                            This book is in your library
+                        </div>
+                        <div
+                            v-else-if="book.list === BookListEnum.Values.WISHLIST"
+                            class="flex items-center gap-2 text-muted-foreground"
+                        >
+                            <Scroll class="size-4" />
+                            This book is on your wishlist
+                        </div>
+                    </div>
+                    <div
+                        class="flex items-center gap-4"
+                    >
+                        <div
+                            v-if="book.events?.find((event) => event.event === BookEventTypeEnum.Values.READ)"
+                            class="flex items-center gap-2 text-success"
+                        >
+                            <BookCheck class="size-4" />
+                            You've read the book
+                        </div>
+                        <div
+                            v-else
+                            class="flex items-center gap-2 text-muted-foreground"
+                        >
+                            <BookDashed class="size-4" />
+                            You haven't read this book yet
+                        </div>
+                    </div>
+
+                    <div class="flex items-center gap-x-4 gap-y-2">
                         <Button
+                            v-if="!book.events?.find((event) => event.event === BookEventTypeEnum.Values.BOUGHT)"
+                            :disabled="isLoading"
                             variant="outline"
                             size="sm"
-                            @click="showReallyDelete=false"
+                            @click="addToLibrary()"
                         >
-                            No
+                            <Library class="size-4" />
+                            Add to library
                         </Button>
+                        <Button
+                            :disabled="isLoading"
+                            variant="outline"
+                            size="sm"
+                            @click="markAsRead()"
+                        >
+                            <BookCheck class="size-4" />
+                            Read
+                        </Button>
+                        <CheckInput
+                            v-model="moreOptions"
+                            label="More options"
+                        />
+                    </div>
+
+                    <div class="divide-y">
+                        <div
+                            v-for="event, index in book.events"
+                            :key="index"
+                            class="flex flex-wrap gap-2 py-2"
+                        >
+                            <Button
+                                v-if="moreOptions"
+                                :disabled="isLoading"
+                                variant="destructive-outline"
+                                size="icon"
+                                @click="removeEvent(index)"
+                            >
+                                <Trash2 class="size-4" />
+                            </Button>
+                            <Badge
+                                variant="secondary"
+                                class="me-auto"
+                            >
+                                {{ event.event }}
+                            </Badge>
+                            <DateInput v-model="event.date" />
+                            <Button
+                                v-if="event.date"
+                                :disabled="isLoading"
+                                variant="ghost"
+                                size="icon"
+                                @click="event.date = null; updateEvents()"
+                            >
+                                <X class="size-4" />
+                            </Button>
+                        </div>
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <Button
+                            v-if="moreOptions"
+                            variant="destructive-outline"
+                            size="sm"
+                            @click="showReallyDelete = !showReallyDelete"
+                        >
+                            <Trash2 class="size-4" />
+                            Delete
+                        </Button>
+                        <div
+                            v-if="showReallyDelete"
+                            class="flex items-center gap-2"
+                        >
+                            Do you really want to delete this book?
+                            <Button
+                                variant="destructive-outline"
+                                size="sm"
+                                :disabled="isLoading"
+                                @click="removeBook()"
+                            >
+                                Yes
+                            </Button>
+                            <Button
+                                variant="outline"
+                                size="sm"
+                                @click="showReallyDelete=false"
+                            >
+                                No
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div
-                v-if="vi.description"
-                class="prose"
-                v-html="vi.description"
-            />
-            <Table>
-                <TableBody>
-                    <TableRow v-if="vi.authors">
-                        <TableHead>Author(s)</TableHead>
-                        <TableCell>{{ vi.authors.join(', ') }}</TableCell>
-                    </TableRow>
-                    <TableRow v-if="vi.publisher">
-                        <TableHead>Publisher</TableHead>
-                        <TableCell>{{ vi.publisher }}</TableCell>
-                    </TableRow>
-                    <TableRow v-if="vi.publishedDate">
-                        <TableHead>Published date</TableHead>
-                        <TableCell>{{ vi.publishedDate }}</TableCell>
-                    </TableRow>
-                    <TableRow v-if="vi.pageCount">
-                        <TableHead>Pages</TableHead>
-                        <TableCell>{{ vi.pageCount }}</TableCell>
-                    </TableRow>
-                    <TableRow v-if="vi.categories">
-                        <TableHead>Pages</TableHead>
-                        <TableCell>{{ vi.categories.join(', ') }}</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+            <Collapsible v-model:open="isOpen">
+                <CollapsibleTrigger
+                    as-child
+                >
+                    <Button variant="outline">
+                        Show details
+                        <ChevronsUpDown class="size-4" />
+                    </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent class="mt-4 grid gap-4">
+                    <div
+                        v-if="vi.description"
+                        class="prose"
+                        v-html="vi.description"
+                    />
+                    <Table>
+                        <TableBody>
+                            <TableRow v-if="vi.authors">
+                                <TableHead>Author(s)</TableHead>
+                                <TableCell>{{ vi.authors.join(', ') }}</TableCell>
+                            </TableRow>
+                            <TableRow v-if="vi.publisher">
+                                <TableHead>Publisher</TableHead>
+                                <TableCell>{{ vi.publisher }}</TableCell>
+                            </TableRow>
+                            <TableRow v-if="vi.publishedDate">
+                                <TableHead>Published date</TableHead>
+                                <TableCell>{{ vi.publishedDate }}</TableCell>
+                            </TableRow>
+                            <TableRow v-if="vi.pageCount">
+                                <TableHead>Pages</TableHead>
+                                <TableCell>{{ vi.pageCount }}</TableCell>
+                            </TableRow>
+                            <TableRow v-if="vi.categories">
+                                <TableHead>Pages</TableHead>
+                                <TableCell>{{ vi.categories.join(', ') }}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CollapsibleContent>
+            </Collapsible>
         </template>
 
         <template #footer>
@@ -164,32 +209,53 @@
 </template>
 
 <script setup lang="ts">
-import { BookCheck,
-         BookDashed, CheckCircle, Scroll,
-         Trash2, } from 'lucide-vue-next'
+import { getLocalTimeZone, today, } from '@internationalized/date'
+import { BookCheck, BookDashed, CheckCircle, ChevronsUpDown, Library, Scroll, Trash2, X, } from 'lucide-vue-next'
 
 import { deleteBook, patchBook, } from '~/lib/api/book'
-import { type Book, type BookList, BookListEnum, BookSchema,  } from '~/lib/entities/book'
+import { type Book, BookListEnum, BookSchema,  } from '~/lib/entities/book'
+import { BookEventTypeEnum, } from '~/lib/entities/book'
 
 const router = useRouter()
 const dialogStore = useDialogStore()
 const { dialogData: book, }: {dialogData: Ref<Book>} = storeToRefs(dialogStore)
 
 const isLoading = ref<boolean>(false)
+const isOpen = ref<boolean>(false)
+const moreOptions = ref<boolean>(false)
 const showReallyDelete = ref<boolean>(false)
 
 const vi = computed(() => book.value.google_book_data.volumeInfo)
 
-const readBook = async (is_read: boolean) => {
+const addToLibrary = async () => {
     isLoading.value = true
-    const { data, } = await patchBook(book.value.id, { is_read, })
+    const events = book.value.events ?? []
+    const { data, } = await patchBook(book.value.id, {
+        list: BookListEnum.Values.LIBRARY,
+        events: [
+            ...events,
+            {
+                date: today(getLocalTimeZone()).toString(),
+                event: BookEventTypeEnum.Values.BOUGHT,
+            },
+        ],
+    })
     book.value = BookSchema.parse(data)
     isLoading.value = false
 }
 
-const setList = async (list: BookList) => {
+const markAsRead = async () => {
     isLoading.value = true
-    const { data, } = await patchBook(book.value.id, { list, })
+    const events = book.value.events ?? []
+    const { data, } = await patchBook(book.value.id, {
+        events: [
+            ...events,
+            {
+                date: today(getLocalTimeZone()).toString(),
+                event: BookEventTypeEnum.Values.READ,
+            },
+        ],
+    })
     book.value = BookSchema.parse(data)
     isLoading.value = false
 }
@@ -199,5 +265,24 @@ const removeBook = async () => {
     await deleteBook(book.value.id)
     isLoading.value = false
     dialogStore.closeDialog()
+}
+
+const removeEvent = async (index: number) => {
+    isLoading.value = true
+    book.value.events.splice(index, 1)
+    const { data, } = await patchBook(book.value.id, {
+        events: book.value.events,
+    })
+    book.value = BookSchema.parse(data)
+    isLoading.value = false
+}
+
+const updateEvents = async () => {
+    isLoading.value = true
+    const { data, } = await patchBook(book.value.id, {
+        events: book.value.events,
+    })
+    book.value = BookSchema.parse(data)
+    isLoading.value = false
 }
 </script>
